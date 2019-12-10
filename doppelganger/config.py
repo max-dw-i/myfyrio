2d3fcdf5d337ba2953d9ca3987adc3a2b899ce7e
Maxim Shpak <maxim.shpak@posteo.uk>
2019-12-10 23:59:18 +0300

    Add preferences (config) handling

---
+++ doppelganger/config.py
@@ -0,0 +1,88 @@
+'''Copyright 2019 Maxim Shpak <maxim.shpak@posteo.uk>
+
+This file is part of Doppelgänger.
+
+Doppelgänger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Doppelgänger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Doppelgänger. If not, see <https://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------------
+
+Module implementing saving/loading programme's preferences to/from
+a config file
+'''
+
+import logging
+import pickle
+from typing import Any, Dict, Optional
+
+
+config_logger = logging.getLogger('main.config')
+
+###############################Types##############################
+
+Param = str
+Value = Any
+ConfigData = Dict[Param, Value] # preferences
+
+##################################################################
+
+
+class Config:
+    '''Represent 'config' containing programme's preferences'''
+
+    DEFAULT_CONFIG_DATA = {
+        'size': 200,
+        'show_similarity': True,
+        'show_size': True,
+        'show_path': True,
+        'sort': 0,
+        'cache_thumbnails': False,
+        'delete_dirs': False
+    }
+
+    def __init__(self, data: Optional[ConfigData] = None) -> None:
+        if data is None:
+            self.data = self.DEFAULT_CONFIG_DATA.copy()
+        else:
+            self.data = data
+
+    def save(self) -> None:
+        """Save data with preferences into file 'config.p'
+
+        :raise OSError: if something goes wrong during
+                        'config.p' saving attempt
+        """
+
+        try:
+            with open('config.p', 'wb') as f:
+                pickle.dump(self.data, f)
+        except OSError as e:
+            config_logger.error(e)
+            raise OSError(e)
+
+    def load(self) -> None:
+        """Load file 'config.p'
+
+        :raise OSError: if something goes wrong during
+                        'config.p' loading attempt
+        """
+
+        try:
+            with open('config.p', 'rb') as f:
+                self.data = pickle.load(f)
+        except FileNotFoundError as e:
+            self.data = self.DEFAULT_CONFIG_DATA.copy()
+        except (EOFError, OSError, pickle.UnpicklingError) as e:
+            config_logger.error(e)
+            self.data = self.DEFAULT_CONFIG_DATA.copy()
+            raise OSError(e)
---
+++ tests/test_config.py
@@ -0,0 +1,120 @@
+'''Copyright 2019 Maxim Shpak <maxim.shpak@posteo.uk>
+
+This file is part of Doppelgänger.
+
+Doppelgänger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Doppelgänger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Doppelgänger. If not, see <https://www.gnu.org/licenses/>.
+'''
+
+
+import logging
+import pickle
+from unittest import TestCase, mock
+
+from PyQt5 import QtWidgets
+
+from doppelganger import config
+
+# Configure a logger for testing purposes
+logger = logging.getLogger('main')
+logger.setLevel(logging.WARNING)
+if not logger.handlers:
+    nh = logging.NullHandler()
+    logger.addHandler(nh)
+
+# Check if there's QApplication instance already
+app = QtWidgets.QApplication.instance()
+if app is None:
+    app = QtWidgets.QApplication([])
+
+
+class TestConfig(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.DEFAULT_CONFIG_DATA = {
+            'size': 200,
+            'show_similarity': True,
+            'show_size': True,
+            'show_path': True,
+            'sort': 0,
+            'cache_thumbnails': False,
+            'delete_dirs': False
+        }
+
+    def setUp(self):
+        self.c = config.Config()
+
+    def test_init_with_data_arg(self):
+        data = {'test_par': 'test_val'}
+        c = config.Config(data)
+
+        self.assertDictEqual(data, c.data)
+
+    def test_init_default_data_arg(self):
+        self.assertDictEqual(self.DEFAULT_CONFIG_DATA, self.c.data)
+
+    @mock.patch('doppelganger.config.pickle.dump')
+    @mock.patch('doppelganger.config.open')
+    def test_save(self, mock_open, mock_dump):
+        self.c.save()
+
+        mock_dump.assert_called_once()
+
+    @mock.patch('doppelganger.config.pickle.dump')
+    @mock.patch('doppelganger.config.open', side_effect=OSError)
+    def test_save_if_OSError(self, mock_open, mock_dump):
+        with self.assertRaises(OSError):
+            with self.assertLogs('main.config', 'ERROR'):
+                self.c.save()
+
+    @mock.patch('doppelganger.config.pickle.load', return_value='test')
+    @mock.patch('doppelganger.config.open')
+    def test_load(self, mock_open, mock_dump):
+        self.c.load()
+
+        self.assertEqual('test', self.c.data)
+
+    @mock.patch('doppelganger.config.pickle.load')
+    @mock.patch('doppelganger.config.open', side_effect=FileNotFoundError)
+    def test_load_if_FileNotFoundError(self, mock_open, mock_dump):
+        self.c.load()
+
+        self.assertEqual(self.c.DEFAULT_CONFIG_DATA, self.c.data)
+
+    @mock.patch('doppelganger.config.pickle.load')
+    @mock.patch('doppelganger.config.open', side_effect=EOFError)
+    def test_load_if_EOFError(self, mock_open, mock_dump):
+        with self.assertRaises(OSError):
+            with self.assertLogs('main.config', 'ERROR'):
+                self.c.load()
+
+        self.assertEqual(self.DEFAULT_CONFIG_DATA, self.c.data)
+
+    @mock.patch('doppelganger.config.pickle.load')
+    @mock.patch('doppelganger.config.open', side_effect=OSError)
+    def test_load_if_OSError(self, mock_open, mock_dump):
+        with self.assertRaises(OSError):
+            with self.assertLogs('main.config', 'ERROR'):
+                self.c.load()
+
+        self.assertEqual(self.DEFAULT_CONFIG_DATA, self.c.data)
+
+    @mock.patch('doppelganger.config.pickle.load', side_effect=pickle.UnpicklingError)
+    @mock.patch('doppelganger.config.open')
+    def test_load_if_UnpicklingError(self, mock_open, mock_dump):
+        with self.assertRaises(OSError):
+            with self.assertLogs('main.config', 'ERROR'):
+                self.c.load()
+
+        self.assertEqual(self.DEFAULT_CONFIG_DATA, self.c.data)
