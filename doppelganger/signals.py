49a174e2b4f9b9fc38e4339f23ce2cfe12d92b08
Maxim Shpak <maxim.shpak@posteo.uk>
2020-01-16 02:22:05 +0300

    Change 'signals.py' docstring

--- doppelganger/signals.py
+++ doppelganger/signals.py
@@ -1,17 +1,38 @@
-'''Custom signals'''
+'''Copyright 2019-2020 Maxim Shpak <maxim.shpak@posteo.uk>
+
+This file is part of Doppelgänger.
+
+Doppelgänger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Doppelgänger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Doppelgänger. If not, see <https://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------------
+
+Module implementing custom signals
+'''
 
 from PyQt5 import QtCore
+
 
 class Signals(QtCore.QObject):
     '''Supported signals:
     ------------------
-    :update_info: label to update: str, text to set: str,
-    :update_progressbar: new progress bar's value: float,
-    :error: error traceback: str,
-    :result: groups of duplicate images: List[core.Group],
-    :finished: processing is done,
-    :interrupted: image processing must be stopped,
-    :clicked: DuplicateWidget is clicked on
+    :signal update_info:        label to update: str, text to set: str,
+    :signal update_progressbar: new value of progress bar: float,
+    :signal error:              error: str,
+    :signal result:             groups of duplicate images: List[core.Group],
+    :signal finished:           processing is done,
+    :signal interrupted:        image processing must be stopped,
+    :signal clicked:            DuplicateWidget is clicked
     '''
 
     update_info = QtCore.pyqtSignal(str, str)
