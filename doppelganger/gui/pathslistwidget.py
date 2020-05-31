6ed1613272d3ec1503a79c79a84f5eb186f3c7f6
Maxim Shpak <maxim.shpak@posteo.uk>
2020-05-31 20:18:56 +0300

    Refactor 'pathsgroupbox.py'

---
+++ doppelganger/gui/pathslistwidget.py
@@ -0,0 +1,92 @@
+'''Copyright 2020 Maxim Shpak <maxim.shpak@posteo.uk>
+
+This file is part of Doppelgänger.
+
+Doppelgänger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Doppelgänger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Doppelgänger. If not, see <https://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------------
+
+Module implementing widget viewing the folders to search for images in
+'''
+
+from typing import List
+
+from PyQt5 import QtCore, QtWidgets
+
+from doppelganger import core
+from doppelganger.gui.multiselectionfiledialog import MultiSelectionFileDialog
+
+
+class PathsListWidget(QtWidgets.QListWidget):
+    '''Widget viewing the folders to search for images in
+
+    :signal hasSelection: True - any path in the widget is selected,
+                          False - otherwise, emitted when the selection
+                          is changed,
+    :signal hasItems:     True - the widget has items (paths),
+                          False - otherwise, emitted when an item (path)
+                          is added or removed
+    '''
+
+    hasSelection = QtCore.pyqtSignal(bool)
+    hasItems = QtCore.pyqtSignal(bool)
+
+    def __init__(self, parent: QtWidgets.QWidget = None):
+        super().__init__(parent)
+
+        self._setSignals()
+
+    def _setSignals(self) -> None:
+        model = self.model()
+        model.rowsInserted.connect(self._hasItems)
+        model.rowsRemoved.connect(self._hasItems)
+
+        self.itemSelectionChanged.connect(self._hasSelectedItems)
+
+    def _hasSelectedItems(self) -> None:
+        if self.selectedItems():
+            self.hasSelection.emit(True)
+        else:
+            self.hasSelection.emit(False)
+
+    def _hasItems(self) -> None:
+        if self.count():
+            self.hasItems.emit(True)
+        else:
+            self.hasItems.emit(False)
+
+    def paths(self) -> List[core.FolderPath]:
+        '''Return all the folders the user added to the widget
+
+        :return: list with the folder paths
+        '''
+
+        return [self.item(i).text() for i in range(self.count())]
+
+    def addPath(self) -> None:
+        '''Open "MultiSelectionFileDialog" to choose folders
+        and add them to the widget
+        '''
+
+        dialog = MultiSelectionFileDialog(self, 'Open Folders', '')
+        if dialog.exec():
+            for path in dialog.selectedFiles():
+                if not self.findItems(path, QtCore.Qt.MatchExactly):
+                    self.addItem(path)
+
+    def delPath(self) -> None:
+        '''Delete the chosen folders from the widget'''
+
+        for item in self.selectedItems():
+            self.takeItem(self.row(item))
---
+++ tests/test_pathslistwidget.py
@@ -0,0 +1,190 @@
+'''Copyright 2020 Maxim Shpak <maxim.shpak@posteo.uk>
+
+This file is part of Doppelgänger.
+
+Doppelgänger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Doppelgänger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Doppelgänger. If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from unittest import TestCase, mock
+
+from PyQt5 import QtCore, QtTest, QtWidgets
+
+from doppelganger.gui import multiselectionfiledialog, pathslistwidget
+
+# Check if there's QApplication instance already
+app = QtWidgets.QApplication.instance()
+if app is None:
+    app = QtWidgets.QApplication([])
+
+
+PLW_MODULE = 'doppelganger.gui.pathslistwidget.'
+
+
+# pylint: disable=missing-class-docstring
+
+
+class TestPathsListWidget(TestCase):
+
+    PLW = PLW_MODULE + 'PathsListWidget'
+
+    def setUp(self):
+        self.w = pathslistwidget.PathsListWidget()
+        self.items = ['path1', 'path2']
+        for item in self.items:
+            self.w.addItem(item)
+
+
+class TestPathsListWidgetMethodInit(TestPathsListWidget):
+
+    def test_setSignals_called(self):
+        with mock.patch(self.PLW+'._setSignals') as mock_signals_call:
+            pathslistwidget.PathsListWidget()
+
+        mock_signals_call.assert_called_once_with()
+
+
+class TestPathsListWidgetMethodSetSignals(TestPathsListWidget):
+
+    def test_model_rowsInserted_signal_connected_to_hasItems(self):
+        mock_model = mock.Mock(spec=QtCore.QAbstractItemModel)
+        with mock.patch(self.PLW+'.model', return_value=mock_model):
+            self.w._setSignals()
+
+        mock_model.rowsInserted.connect.assert_called_once_with(
+            self.w._hasItems
+        )
+
+    def test_model_rowsRemoved_signal_connected_to_hasItems(self):
+        mock_model = mock.Mock(spec=QtCore.QAbstractItemModel)
+        with mock.patch(self.PLW+'.model', return_value=mock_model):
+            self.w._setSignals()
+
+        mock_model.rowsRemoved.connect.assert_called_once_with(
+            self.w._hasItems
+        )
+
+    def test_itemSelectionChanged_signal_connected_to_hasSelectedItems(self):
+        self.w.itemSelectionChanged = mock.Mock()
+        self.w._setSignals()
+
+        self.w.itemSelectionChanged.connect.assert_called_once_with(
+            self.w._hasSelectedItems
+        )
+
+
+class TestPathsListWidgetMethodHasSelectedItems(TestPathsListWidget):
+
+    def test_emit_hasSelection_signal_with_True_if_any_item_selected(self):
+        self.w.item(0).setSelected(True)
+        spy = QtTest.QSignalSpy(self.w.hasSelection)
+        self.w._hasSelectedItems()
+
+        self.assertEqual(len(spy), 1)
+        self.assertTrue(spy[0][0])
+
+    def test_emit_hasSelection_signal_with_False_if_no_item_selected(self):
+        spy = QtTest.QSignalSpy(self.w.hasSelection)
+        self.w._hasSelectedItems()
+
+        self.assertEqual(len(spy), 1)
+        self.assertFalse(spy[0][0])
+
+
+class TestPathsListWidgetMethodHasItems(TestPathsListWidget):
+
+    def test_emit_hasItems_signal_with_True_if_there_are_items(self):
+        spy = QtTest.QSignalSpy(self.w.hasItems)
+        self.w._hasItems()
+
+        self.assertEqual(len(spy), 1)
+        self.assertTrue(spy[0][0])
+
+    def test_emit_hasItems_signal_with_False_if_there_is_no_items(self):
+        for _ in range(self.w.count()):
+            self.w.takeItem(0)
+        spy = QtTest.QSignalSpy(self.w.hasItems)
+        self.w._hasItems()
+
+        self.assertEqual(len(spy), 1)
+        self.assertFalse(spy[0][0])
+
+
+class TestPathsListWidgetMethodPaths(TestPathsListWidget):
+
+    def test_paths_return(self):
+        res = self.w.paths()
+
+        self.assertListEqual(res, self.items)
+
+
+class TestPathsListWidgetMethodAddPath(TestPathsListWidget):
+
+    DIALOG = PLW_MODULE + 'MultiSelectionFileDialog'
+
+    def setUp(self):
+        super().setUp()
+
+        self.mock_dialog = mock.Mock(
+            multiselectionfiledialog.MultiSelectionFileDialog
+        )
+
+    def _paths(self):
+        return [self.w.item(i).text() for i in range(self.w.count())]
+
+    def test_MultiSelectionFileDialog_called(self):
+        with mock.patch(self.DIALOG) as mock_dialog_call:
+            self.w.addPath()
+
+        mock_dialog_call.assert_called_once_with(self.w, 'Open Folders', '')
+
+    def test_paths_not_changed_if_nothing_selected(self):
+        self.mock_dialog.exec.return_value = 0
+        with mock.patch(self.DIALOG, return_value=self.mock_dialog):
+            self.w.addPath()
+
+        self.assertListEqual(self._paths(), self.items)
+
+    def test_paths_not_changed_if_selected_already_added(self):
+        self.mock_dialog.exec.return_value = 1
+        self.mock_dialog.selectedFiles.return_value = self.items[:1]
+        with mock.patch(self.DIALOG, return_value=self.mock_dialog):
+            self.w.addPath()
+
+        self.assertListEqual(self._paths(), self.items)
+
+    def test_paths_changed_if_selected_not_already_added(self):
+        self.mock_dialog.exec.return_value = 1
+        self.mock_dialog.selectedFiles.return_value = ['path3']
+        with mock.patch(self.DIALOG, return_value=self.mock_dialog):
+            self.w.addPath()
+
+        self.assertListEqual(self._paths(), self.items + ['path3'])
+
+
+class TestPathsListWidgetMethodDelPath(TestPathsListWidget):
+
+    def test_selected_folder_deleted(self):
+        self.w.item(len(self.items)-1).setSelected(True) # select second item
+        self.w.delPath()
+        items_after_del = [self.w.item(i).text()
+                           for i in range(self.w.count())]
+
+        self.assertListEqual(items_after_del, self.items[:-1])
+
+    def test_nothing_deleted_if_nothing_selected(self):
+        self.w.delPath()
+        items_after_del_run = [self.w.item(i).text()
+                               for i in range(self.w.count())]
+
+        self.assertListEqual(items_after_del_run, self.items)
