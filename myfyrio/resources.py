eb0c45d1503726d6bd2169dfbd1f138f28bdb63c
Maxim Shpak <maxim.shpak@posteo.uk>
2020-06-24 23:58:49 +0300

    Rename error image

--- myfyrio/resources.py
+++ myfyrio/resources.py
@@ -52,7 +52,7 @@ class Image(Resource):
     '''Enum class representing images and the paths to them'''
 
     ICON = 'myfyrio/static/images/icon.png'
-    ERR_IMG = 'myfyrio/static/images/image_error.png'
+    ERR_IMG = 'myfyrio/static/images/error.png'
 
 
 class Config(Resource):
