78f7886fee0c4f0e128b7f420361ccd7d483bfc1
Maxim Shpak <maxim.shpak@posteo.uk>
2020-10-02 22:13:09 +0300

    Add copyright notices into some files

--- deploy/setvarsmsvc.bat
+++ deploy/setvarsmsvc.bat
@@ -1,3 +1,26 @@
+@echo off
+REM MIT License
+
+REM Copyright (c) 2020 Maxim Shpak <maxim.shpak@posteo.uk>
+
+REM Permission is hereby granted, free of charge, to any person obtaining a
+REM copy of this software and associated documentation files (the "Software"),
+REM to deal in the Software without restriction, including without limitation
+REM the rights to use, copy, modify, merge, publish, distribute, sublicense,
+REM and/or sell copies of the Software, and to permit persons to whom
+REM the Software is furnished to do so, subject to the following conditions:
+
+REM The above copyright notice and this permission notice shall be included in
+REM all copies or substantial portions of the Software.
+
+REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+REM AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+REM FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+REM DEALINGS IN THE SOFTWARE.
+
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 SET qt_src_dir=%1
 SET PATH=%qt_src_dir%\bin;%qt_src_dir%\gnuwin32\bin;%PATH%'
